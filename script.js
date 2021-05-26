4a8509c9548e793598827bda5653a253497dd78c
Alberto Pluecker <albertopluecker@gmail.com>
2021-05-26 21:35:31 +0200

    + Buttons + Basic calculation functions + Clear, Operation and display functions

--- script.js
+++ script.js
@@ -1,15 +1,74 @@
-var outputScreen = document.querySelector('#outputScreen');
- 
- function add(a,b){
-    return a + b;
+const clearButton   =   document.querySelector("[button-clear]");
+const equalsButton  =   document.querySelector("[button-equals]");
+const operators     =   document.querySelectorAll("[operator-button]");
+const numberButtons =   document.querySelectorAll("[number-button]");
+const deleteButton  =   document.querySelector("[button-delete]");
+const pointButton   =   document.querySelector("[point-button]");
+const outputScreen  =   document.querySelector('#outputScreen');
+
+
+let operand1   =  "";
+let operand2   =  "";
+let currentOperation  = null;
+let shouldResetDisplay = false;
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => outputConcat(button.textContent))
+);
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => setOperand(button.textContent))
+);
+
+operators.forEach((button) =>
+  button.addEventListener("click", () => outputConcat(button.textContent))
+);
+
+operators.forEach((button) =>
+  button.addEventListener("click", () => currentOperation = button.textContent)
+);
+
+
+
+
+clearButton.addEventListener("click", clrScreen);
+equalsButton.addEventListener("click", operate);
+
+
+//Concatenates output to show the calculation being processed on screen
+function outputConcat(value) {
+  if(outputScreen.textContent == 0) resetDisplay();
+  outputScreen.textContent = outputScreen.textContent + " " + value + " ";
 }
 
-function subtract(a,b){
-    return a - b;
+// output to calculator screen
+function outputToScreen(output) {
+    outputScreen.textContent = output;
 }
 
-function multiply(a,b){
-    return a * b;
+function resetDisplay() {
+    outputScreen.textContent = "";
+    shouldResetDisplay      = false;    
+  }
+
+//Returns variables to their initial values before the user can start a new operation
+function clrScreen(){
+    outputScreen.textContent = "0";
+    operand1          = "";
+    operand2          = "";
+    currentOperation  = null;
+    shouldResetDisplay= false;   
+}
+
+//Sets the current operands
+function setOperand(value){
+  if(operand1 = null){
+    operand1 = value;
+  }
+  else if (operand2 = null){
+    operand2 = value;
+    shouldResetDisplay = true;
+  }
 }
 
 function divide(a,b){
@@ -21,24 +80,26 @@ function divide(a,b){
 }
 
 function operate(operator,operand1,operand2) {
-        switch (operator) {
-            case add:
-                return add(operand1,operand2);
-                break;
-            case subtract:
-                return subtract(operand1,operand2);
-                break;
-
-            case multiply:
-                return multiply(operand1,operand2);
-                break;
-            case divide:
-                return divide(operand1,operand2);
-                break;
-            //default:
-                //break;
-        }
+    x = Number(operand1);
+    y = Number(operand2);
+    switch (operator) {
+        case "+":
+          outputToScreen(a + b);
+          operand1 = a + b;
+        case "−":
+          return a - b;
+          operand1 = a - b;
+        case "×":
+          return a * b;
+          operand1 = a * b;
+        case "÷":
+          if (b === 0) return null;
+          else return divide(a,b);
+          operand1 = divide(a,b);
+        default:
+          return null;
+}
 }
 
-var outputScreen = document.querySelector('#outputScreen');
+
 
